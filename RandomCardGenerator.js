662527343a63f1fd7e37239f7a14c063ee6bcae3
guillermosequera <sequeracrespo@gmail.com>
2020-07-21 06:55:16 -0400

    end

--- RandomCardGenerator.js
+++ RandomCardGenerator.js
@@ -7,12 +7,6 @@ Numbers = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
 let  TheCard = Suits[Math.floor(Math.random() * Suits.length)];
 let  TheNumber = Numbers[Math.floor(Math.random() * Numbers.length)];
 
-// if (Suits[theCard]=== "\2665" || Suits[theCard]==="\2666"){
-//     document.getElementsByClassName('numbers').classList.add('blackSuit')
-// } else {
-//     document.getElementsByClassName('numbers').classList.add('blackSuit')
-// }
-
 
 card.classList.add(TheCard);
 number.innerHTML = TheNumber;
